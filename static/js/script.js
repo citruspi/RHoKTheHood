62dddb692bd6f0883e07901256e58ac1f06b3cb1
Mihir Singh <me@mihirsingh.com>
2013-12-08 10:59:56 -0500

    Added a list of filters

--- static/js/script.js
+++ static/js/script.js
@@ -41,6 +41,63 @@ $(document).ready(function () {
     });
 });
 
+var filterOptions = [
+
+    "% Bachelors or More",
+    "% Commercial Land Use",
+    "% High School or More",
+    "% In Labor Force",
+    "% Industrial Land Use",
+    "% Other Land Use",
+    "% Owner Occupied",
+    "% Renter Occupied",
+    "% Residential Land Use",
+    "% School Aged Children",
+    "% Vacant Land Use",
+    "% on Public Assistance",
+    "All Other Population",
+    "American Indian Population",
+    "Animal Calls Rate",
+    "Asian Population",
+    "Assessed Value 2012",
+    "Black Population",
+    "Blighted Properties",
+    "Block Group ID",
+    "Certs of Compliance/Occupancy Issued",
+    "Child Poverty Rate",
+    "City Demolitions",
+    "Estimated Tax Bill 2012",
+    "Forclosure Starts",
+    "Household Size",
+    "Households",
+    "Land Value Per Acre 2012",
+    "Median Household Income",
+    "Nuisance Calls Rate",
+    "Out of Town Owners",
+    "Parcels",
+    "PopDen",
+    "Population",
+    "Population 10-17",
+    "Population 18-21",
+    "Population 22-29",
+    "Population 30's",
+    "Population 40's",
+    "Population 50's",
+    "Population 60's";
+    "Population Over 70",
+    "Population Under 9",
+    "Poverty Rate",
+    "Property Crime Rate",
+    "School Kids",
+    "Square Miles",
+    "Unemployment Rate",
+    "Vacant Lots",
+    "Vacant Structures",
+    "Vice Calls Rate",
+    "Violent Crime Rate",
+    "White Population"
+];
+
 function populateScale (label) {
     stats[label] = stats[label].map(Number);
     var min = d3.min(stats[label]),
