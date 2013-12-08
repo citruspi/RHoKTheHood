83b77e4af98a952c062aca71934ca7ce83719896
mpn3712 <mpn3712@rit.edu>
2013-12-08 09:37:13 -0500

    added correct opacity coloring :punch:

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+var blocks = [];
+
 $(document).ready(function () {
     var main = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data © OpenStreetMap contributors',
@@ -8,28 +10,50 @@ $(document).ready(function () {
         current_towns = L.layerGroup();
 
     window.map = L.map('map-canvas', {
-        center: [42.6501, -76.3659],
-        zoom: 7,
+        center: [43.160552395407585, -77.60656356811523],
+        zoom: 13,
         layers: [
             main
          ]
     });
 
    $.getJSON('/static/data/US/NY/055/US-NY-055-SHAPE.json', function (features) {
-        $.getJSON('/static/data/US/NY/055/US-NY-055-STATS.json', function (stats) {
+        csvToJson('/static/data/US/NY/055/US-NY-055-STATS.csv', function (stats) {
             L.geoJson(features, { 
                 style: function (feature) {
                     var id = feature.properties['GEOID'];
                            return {
                                fillColor: "blue",
-                               fillOpacity: 0.4
+                               fillOpacity: 0
                             }
                 },
                 onEachFeature: function (feature, layer) {
-
+                    layer.index = stats['Block Group ID'].indexOf(feature.properties.GEOID);
+                    window.stats = stats;
+                    if(layer.index !== -1) {
+                        blocks.push(layer);
+                    }                    
                 }
             }).addTo(map);
+            populateScale('Violent Crime Rate')
 
         });
     });
-})
+});
+
+function populateScale (label) {
+    stats[label] = stats[label].map(Number);
+    var min = d3.min(stats[label]),
+        max = d3.max(stats[label]),
+        scale = d3.scale.linear();
+    scale.domain([min, max])
+        .range([0, 0.9]);
+
+    blocks.forEach(function (block) {
+        var val = parseFloat(stats[label][block.index]),
+            o = scale(val);
+        //console.log(o, val, min, max);
+        block.setStyle({fillOpacity: o});
+    });
+    console.log(stats[label]);
+}
